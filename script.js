486106cf7b80a92813f7d1cd8bfa19083a67c12b
ymalakhov <it.malakhov@gmail.com>
2022-03-18 22:19:47 +0100

    submit btn now works

--- script.js
+++ script.js
@@ -1,74 +1,77 @@
 const listOfTasks = document.querySelector("ul");
 const form = document.querySelector(".addTask");
 const count = document.querySelector("#count");
+const submitBtn = document.querySelector("#button-addon2");
 
 let revengesCount = 0;
 
-const tasks = [{
-        name: "Find a person",
-    },
-    {
-        name: "Make him/her pay! Online",
-    },
-    {
-        name: "But not in real live",
-    },
+const tasks = [
+  {
+    name: "Find a person",
+  },
+  {
+    name: "Make him/her pay! Online",
+  },
+  {
+    name: "But not in real live",
+  },
 ];
 
 function addTask(task) {
-    const template = `
+  const template = `
       <li class="task">
         <span>${task.name}</span> 
         <img src="assets/icons/fire.svg" id="fire">
       </li>
     `;
 
-    listOfTasks.innerHTML += template;
+  listOfTasks.innerHTML += template;
 }
 
 function renderTasks() {
-    tasks.forEach((task) => addTask(task));
+  tasks.forEach((task) => addTask(task));
 }
 
 function handleNewTask(event) {
-    event.preventDefault();
-    const task = form.add.value.trim();
-    if (task) {
-        tasks.push({ name: task });
-        clearAndRender();
+  event.preventDefault();
+  const task = form.add.value.trim();
+  if (task) {
+    tasks.push({ name: task });
+    clearAndRender();
 
-        form.reset();
-    } else {
-        alert("Please enter a target!");
-    }
+    form.reset();
+  } else {
+    alert("Please enter a target!");
+  }
 }
 
 function clearAndRender() {
-    clearTasks(listOfTasks);
-    renderTasks();
+  clearTasks(listOfTasks);
+  renderTasks();
 }
 
 listOfTasks.addEventListener("click", (e) => {
-    if (e.target.id === "fire") {
-        e.target.parentElement.remove();
-        revengesCount++;
-        count.textContent = revengesCount;
-    } else {
-        console.log("");
-    }
+  if (e.target.id === "fire") {
+    e.target.parentElement.remove();
+    revengesCount++;
+    count.textContent = revengesCount;
+  } else {
+    console.log("");
+  }
 });
 
 function clearTasks(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    }
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
 }
 
 console.log(form);
 form.addEventListener("submit", handleNewTask);
+submitBtn.addEventListener("click", handleNewTask);
 
 listOfTasks.addEventListener("click", (e) => {
-    e.target.classList.toggle("active");
+  e.target.classList.toggle("active");
 });
 
 renderTasks();
